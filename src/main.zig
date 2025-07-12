aaae96241faed1c78f336e60197fc78bd33d20fe
Marc Soliman <71621832+MarcSoliman@users.noreply.github.com>
2025-07-12 18:53:16 -0500

    simple indenting and formatting to output file

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
+const dir = std.fs.Dir;
 const json = @import("std").json;
+const utils = @import("utils.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
     const path = "input.json";
+    const outputPath = "./output";
 
     // Opening the file
     const file = try std.fs.cwd().openFile(path, .{});
@@ -18,6 +21,11 @@ pub fn main() !void {
     var parsed = try json.parseFromSlice(json.Value, allocator, contents, .{});
     defer parsed.deinit();
 
-    // print contents for now
-    try json.stringify(parsed.value, .{ .whitespace = .indent_4 }, std.io.getStdOut().writer());
+    var outputDir = try utils.ensureDirExists(outputPath);
+    defer outputDir.close();
+
+    var outputFile = try outputDir.createFile("output.json", .{ .truncate = true });
+    defer outputFile.close();
+
+    _ = try json.stringify(parsed.value, .{ .whitespace = .indent_4 }, outputFile.writer());
 }
